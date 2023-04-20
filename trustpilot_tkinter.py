4cc8cad52934a29512c53285023d11aaf38bec65
Andreea Spiridon <andreeaspiridon@Andreeas-Air.lan>
2023-04-20 19:48:35 +0300

    adds new listbox and functionality

--- trustpilot_tkinter.py
+++ trustpilot_tkinter.py
@@ -7,22 +7,26 @@ sql_path = "trustpilot.db"
 with sqlite3.connect(sql_path) as db:
     cursor = db.cursor()
     print('Connected to db')
-location = cursor.execute("""SELECT location FROM trustpilot_table""").fetchall()
+# location = cursor.execute("""SELECT location FROM trustpilot_table""").fetchall()
 
 
 class TrustPilotTK:
     def __init__(self, master):
+        self.list3 = None
+        self.entry3 = None
+        self.trust_score = None
+        self.entry3_str = None
+        self.services = None
+        self.location = None
         self.label2 = None
         self.label1 = None
         self.list2 = None
         self.entry2 = None
-        self.service = None
         self.entry2_str = None
         self.value = None
         self.selected_item = None
         self.entry1_str = None
         self.entry1 = None
-        self.loc = None
         self.list1 = None
         self.master = master
         self.render_main_page()
@@ -32,6 +36,8 @@ class TrustPilotTK:
         search_str = entry_name.get()  # user entered string
         list_name.delete(0, END)
         for element in elements:
+            if isinstance(element, float):
+                element = str(element)
             if re.search(search_str, element, re.IGNORECASE):
                 list_name.insert(END, element.title())
 
@@ -43,24 +49,30 @@ class TrustPilotTK:
         entry_name.set(selected_value.title())
         list_name.delete(0, END)
 
+
     def render_main_page(self):
         self.master.resizable(False, False)
         self.master.title("UK services")
         self.master.eval("tk::PlaceWindow . center")  # place window at center of screen
-        self.master.geometry('560x400')
+        self.master.geometry('700x400')
         self.master.config(background="#B4B4EE")
-        font1 = ('Times', 24, 'bold')
-        font2 = ('Times', 20, 'bold')
+        font1 = ('Times', 20, 'bold')
 
         # Labels
-        self.label1 = tk.Label(self.master, text="Select location", font=font2, bg="#B4B4EE", fg='black')
+        self.label1 = tk.Label(self.master, text="Select location", font=font1, bg="#B4B4EE", fg='black')
         self.label1.grid(row=0, column=1, padx=20, pady=5)
-        self.label2 = tk.Label(self.master, text="Select services", font=font2, bg="#B4B4EE", fg='black')
+        self.label2 = tk.Label(self.master, text="Select services", font=font1, bg="#B4B4EE", fg='black')
         self.label2.grid(row=0, column=2, padx=20, pady=5)
+        self.label3 = tk.Label(self.master, text="Select trust score", font=font1, bg="#B4B4EE", fg='black')
+        self.label3.grid(row=0, column=3, padx=30, pady=5)
+
+
 
         # Entry 1 - LOCATION
         self.entry1_str = tk.StringVar(self.master)
-        self.loc = ['Select Location', 'Loc 1 ', 'Loc 2', 'Loc 3', 'Loc 101010']
+
+        self.location = set([x[0] for x in cursor.execute("""SELECT location FROM trustpilot_table""").fetchall()])
+        # print(self.location)
 
         self.entry1 = tk.Entry(self.master, font=font1, textvariable=self.entry1_str)
         self.entry1.grid(row=1, column=1, padx=20,pady=5)
@@ -68,7 +80,7 @@ class TrustPilotTK:
                              bg="#B4B4EE", highlightcolor="#B4B4EE")
         self.list1.grid(row=2, column=1)
 
-        self.entry1_str.trace('w', lambda *args:  self.get_data(self.loc,
+        self.entry1_str.trace('w', lambda *args:  self.get_data(self.location,
                                                                 self.entry1,
                                                                 self.list1))
         self.list1.bind("<<ListboxSelect>>", lambda event: self.search_item(event,
@@ -77,18 +89,52 @@ class TrustPilotTK:
 
         # Entry 2 - SERVICES
         self.entry2_str = tk.StringVar(self.master)
-        self.service = ['fitness', 'restaurant', 'spa', 'nail salon']
+        # self.service = ['fitness', 'restaurant', 'spa', 'nail salon']
+        # query = "SELECT title FROM trustpilot_table WHERE services LIKE ?"
+        # rez = cursor.execute(query, ("%{}%".format(self.entry2),))
+        # self.services = set([x[0] for x in rez.fetchall()])
+
+        self.services = set([x[0] for x in cursor.execute("""SELECT services FROM trustpilot_table""").fetchall()])
+
         self.entry2 = tk.Entry(self.master, font=font1, textvariable=self.entry2_str)
         self.entry2.grid(row=1, column=2)
         self.list2 = tk.Listbox(self.master, height=6, font=font1, relief='flat',
                                 bg="#B4B4EE", highlightcolor="#B4B4EE")
         self.list2.grid(row=2, column=2)
-        self.entry2_str.trace('w', lambda *args:  self.get_data(self.service,
+
+
+
+        self.entry2_str.trace('w', lambda *args:  self.get_data(self.services,
                                                                 self.entry2,
                                                                 self.list2))
         self.list2.bind("<<ListboxSelect>>", lambda event: self.search_item(event,
                                                                             self.entry2_str,
                                                                             self.list2))
+
+        # Entry 3 - TrustScore
+        self.entry3_str = tk.StringVar(self.master)
+        self.trust_score = set([x[0] for x in cursor.execute("""SELECT trust_score FROM trustpilot_table""").fetchall()])
+        print(self.trust_score)
+
+        self.entry3 = tk.Entry(self.master, width=10, font=font1, textvariable=self.entry3_str)
+        self.entry3.grid(row=1, column=3)
+        self.list3 = tk.Listbox(self.master, height=6, font=font1, relief='flat',
+                                bg="#B4B4EE", highlightcolor="#B4B4EE")
+        self.list3.grid(row=2, column=3)
+        self.entry3_str.trace('w', lambda *args:  self.get_data(self.trust_score,
+                                                                    self.entry3,
+                                                                    self.list3))
+        self.list3.bind("<<ListboxSelect>>", lambda event: self.search_item(event,
+                                                                            self.entry3_str,
+                                                                            self.list3))
+        # TODO
+        '''
+        de adaugat un buton pentru returnarea rezultatului
+        si de adaugat un textbox in care vor fi enumerate rezultatele in
+        urma combinarii celor 3 entry-uri 
+        '''
+
+
 
 
 if __name__ == '__main__':
